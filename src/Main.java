788dc324ca2e6f0bce9405087eb1db50476f12ad
Zuzana Tománková <Tomankova.Z@seznam.cz>
2024-02-10 18:39:40 +0100

    použití otherGuest

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import com.engeto.ja.hotel.Booking;
+import com.engeto.ja.hotel.Guest;
+import com.engeto.ja.hotel.Room;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Guest guest1 = new Guest("Adéla","Malíková", LocalDate.of(1993,3,13));
+        Guest guest2 = new Guest("Jan","Dvořáček", LocalDate.of(1995,5,5));
+
+        List<Guest> otherGuests = new ArrayList<>();
+        otherGuests.add(guest2);
+
+        List <Guest> guestList = new ArrayList<>();
+        guestList.add(guest1);
+        guestList.add(guest2);
+
+
+        Room room1 = new Room(1,1,true,true, BigDecimal.valueOf(1000));
+        Room room2 = new Room(2,1,true,true, BigDecimal.valueOf(1000));
+        Room room3 = new Room(3,3,false,true, BigDecimal.valueOf(2400));
+
+        Booking booking1 = new Booking(room1,guest1,null,LocalDate.of(2021,7,19),LocalDate.of(2021,7,26),true);
+        Booking booking2 = new Booking(room3,guest1,otherGuests,LocalDate.of(2021,7,19),LocalDate.of(2021,7,26),true);
+
+        List<Booking>bookingList = new ArrayList<>();
+        bookingList.add(booking1);
+        bookingList.add(booking2);
+
+        System.out.println("Rooms available:");
+        System.out.println(room1+"Kč");
+        System.out.println(room2+"Kč");
+        System.out.println(room3+"Kč");
+
+        bookingList.forEach(System.out::println);
+
+
     }
 }
--- src/com/engeto/ja/hotel/Booking.java
+++ src/com/engeto/ja/hotel/Booking.java
@@ -1,2 +1,90 @@
-package com.engeto.ja.hotel;public class Booking {
+package com.engeto.ja.hotel;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Booking {
+    private Room room;
+    private Guest guest;
+    private List<Guest> otherGuests;
+    private LocalDate startOfStay;
+    private LocalDate endOfStay;
+    private boolean businessStay;
+
+    public Booking(Room room, Guest guest, List<Guest> otherGuests, LocalDate startOfStay, LocalDate endOfStay, boolean businessStay) {
+        this.room = room;
+        this.guest = guest;
+        this.otherGuests = otherGuests;
+        this.startOfStay = startOfStay;
+        this.endOfStay = endOfStay;
+        this.businessStay = businessStay;
+    }
+
+    //region Přístupové metody
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public Guest getGuest() {
+        return guest;
+    }
+
+    public void setGuest(Guest guest) {
+        this.guest = guest;
+    }
+
+    public List<Guest> getOtherGuests() {
+        return new ArrayList<>(otherGuests);
+    }
+
+    public void setOtherGuests(List<Guest> otherGuests) {
+        this.otherGuests = otherGuests;
+
+    }
+
+    public LocalDate getStartOfStay() {
+        return startOfStay;
+    }
+
+    public void setStartOfStay(LocalDate startOfStay) {
+        this.startOfStay = startOfStay;
+    }
+
+    public LocalDate getEndOfStay() {
+        return endOfStay;
+    }
+
+    public void setEndOfStay(LocalDate endOfStay) {
+        this.endOfStay = endOfStay;
+    }
+
+    public boolean isBusinessStay() {
+        return businessStay;
+    }
+
+    public void setBusinessStay(boolean businessStay) {
+        this.businessStay = businessStay;
+    }
+
+
+    //endregion
+
+
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "room=" + room +
+                ", guest=" + guest +
+                ", otherGuests=" + otherGuests +
+                ", startOfStay=" + startOfStay +
+                ", endOfStay=" + endOfStay +
+                ", businessStay=" + businessStay +
+                '}';
+    }
 }
