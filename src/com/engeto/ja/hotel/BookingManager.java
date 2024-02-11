af8b4073ea050e05dd702bb46ebbb6f11eca32f2
Zuzana Tománková <Tomankova.Z@seznam.cz>
2024-02-11 10:08:24 +0100

    přidání metod do BookingManager

---
+++ src/com/engeto/ja/hotel/BookingManager.java
@@ -0,0 +1,36 @@
+package com.engeto.ja.hotel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookingManager {
+
+    private List<Booking> bookingList = new ArrayList<>();
+
+    public void addBooking(Booking booking) {
+        bookingList.add(booking);
+    }
+
+    public void removeBooking(Booking booking) {
+        bookingList.remove(booking);
+    }
+
+    public List<Booking> getBookings() {
+        return new ArrayList<>(bookingList);
+    }
+
+    public void clearBookings(Booking booking){
+        bookingList.clear();
+    }
+
+    public void getNumberOfWorkingBookings(Booking booking){
+        bookingList.size();
+    }
+
+    public void getAverageGuests(Booking booking){
+
+    }
+
+
+
+}
